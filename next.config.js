6c6438b9f846fc2fe8688edd01c6b4decc93e511
wenn <wenn@wenndeMac-mini.local>
2023-04-25 20:37:13 +0800

    test: 💍 all
    all
    
    BREAKING CHANGE: 🧨 all

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 // 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
 // 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
+const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
 let assetPrefix = `/${repo}/`
 
 // 用于为应用设置基础路径
@@ -10,7 +12,6 @@ let basePath = `/${repo}`
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
 if (isGithubActions) {
-	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 	assetPrefix = `/${repo}/`
 	basePath = `/${repo}`
 }
